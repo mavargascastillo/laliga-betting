6f67d75124da2dddd0b294035e2e7fa4482015ab
Miguel Angel Vargas <miguelxtype@gmail.com>
2024-11-12 03:47:47 +0000

    Set API key and URL

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
-# API key for football-data.org
-API_KEY = '3a4c168b2af9421e87b5fcfe5daeef6c'
+import os 
+
+# Retrieve the API key from the environment variable
+FOOTBALL_API_KEY = os.getenv('FOOTBALL_DATA_API_KEY')
 
 # API endpoint URL for fetching La Liga match data
-ENDPOINT_URL = 'https://api.football-data.org/v4/matches'
+ENDPOINT_URL = 'https://api.football-data.org/v4/competitions/PD/matches?matchday=12'
 
 # Path to the CSV file where match data will be stored
 STORAGE_FILE = 'data/laliga_matches.csv'
 
 # Code I got in the email for launching
-headers = { 'X-Auth-Token': '3a4c168b2af9421e87b5fcfe5daeef6c' }
+headers = { 'X-Auth-Token': FOOTBALL_API_KEY }
